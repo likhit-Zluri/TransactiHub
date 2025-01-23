b303045facf3cb5f8a5520380cb82fd4c6986caa
likhit-Zluri <likhit.r@zluri.com>
2025-01-23 19:56:28 +0530

    v

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
 		<StrictMode>
+			<SpeedInsights />
 			<App />
 		</StrictMode>
 	</BrowserRouter>
