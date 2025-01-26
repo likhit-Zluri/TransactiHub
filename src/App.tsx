814612898d8cc99eeb9852b51737e5c05cea2bff
likhit-Zluri <likhit.r@zluri.com>
2025-01-26 21:45:47 +0530

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import Dashboard from "./pages/Dashboard";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
 	return (
 		<div>
+			<Analytics/>
 			<SpeedInsights />
 			<Dashboard />
 		</div>
